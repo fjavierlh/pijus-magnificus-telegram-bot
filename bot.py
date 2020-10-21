2abc85ffd1bc973e4f74740e43f5b4bbcf79825d
fjavierlh <fjavierlh@gmail.com>
2020-10-21 18:58:02 +0200

    Printed messages in bot.log to test logging system

--- bot.py
+++ bot.py
@@ -2,8 +2,15 @@ import logging
 from telegram.ext import Updater
 
 logging.basicConfig(
-    filename="bot.log",
+    filename='bot.log',
+    encoding='utf-8',
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s,",
     level= logging.INFO
 )
 logger = logging.getLogger('Pijus Magnificus')
+
+logger.debug('Mensaje para darnos información a l@s desarrollador@s.')
+logger.info('Para información del funcionamiento normal del progama.')
+logger.warning('Algo no ha ido como esperábamos, habrá que echarle un ojo.')
+logger.error('Algo está dando problemas chungos.')
+logger.critical('Esto ha petado.')
